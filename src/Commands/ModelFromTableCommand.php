993062ed6f27c48bef6a4bdded797a9548d234a1
Michael McMullen <811123+michael-mcmullen@users.noreply.github.com>
2020-02-20 13:48:20 -0500

    Formatting :)

--- src/Commands/ModelFromTableCommand.php
+++ src/Commands/ModelFromTableCommand.php
@@ -3,8 +3,8 @@
 namespace Laracademy\Generators\Commands;
 
 use DB;
-use Illuminate\Support\Str;
 use Schema;
+use Illuminate\Support\Str;
 use Illuminate\Console\Command;
 
 class ModelFromTableCommand extends Command
